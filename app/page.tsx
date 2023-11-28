f0b8aa094f002cef5589fec9250488201b6b7b03
Dennis Stolmeijer <dstollie@gmail.com>
2023-11-28 18:46:18 +0100

    Trash

--- app/page.tsx
+++ app/page.tsx
@@ -11,87 +11,26 @@ export const dynamic = 'force-dynamic'
 export default function Home() {
   return (
     <main className="relative flex min-h-screen flex-col items-center justify-center">
-      <Link
-        href="https://vercel.com/templates/next.js/postgres-kysely"
-        className="group mt-20 sm:mt-0 rounded-full flex space-x-1 bg-white/30 shadow-sm ring-1 ring-gray-900/5 text-gray-600 text-sm font-medium px-10 py-2 hover:shadow-lg active:shadow-sm transition-all"
-      >
-        <p>Deploy your own to Vercel</p>
-        <ExpandingArrow />
-      </Link>
-      <h1 className="pt-4 pb-8 bg-gradient-to-br from-black via-[#171717] to-[#575757] bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl">
-        Postgres on Vercel
-      </h1>
-      <Suspense fallback={<TablePlaceholder />}>
-        <Table />
-      </Suspense>
-      <p className="font-light text-gray-600 w-full max-w-lg text-center mt-6">
-        <Link
-          href="https://vercel.com/postgres"
-          className="font-medium underline underline-offset-4 hover:text-black transition-colors"
-        >
-          Vercel Postgres
-        </Link>{' '}
-        demo with{' '}
-        <Link
-          href="https://kysely.dev/"
-          className="font-medium underline underline-offset-4 hover:text-black transition-colors"
-        >
-          Kysely
-        </Link>{' '}
-        as the ORM. <br /> Built with{' '}
-        <Link
-          href="https://nextjs.org/docs"
-          className="font-medium underline underline-offset-4 hover:text-black transition-colors"
-        >
-          Next.js App Router
-        </Link>
-        .
-      </p>
-
-      <div className="flex justify-center space-x-5 pt-10 mt-10 border-t border-gray-300 w-full max-w-xl text-gray-600">
-        <Link
-          href="https://postgres-prisma.vercel.app/"
-          className="font-medium underline underline-offset-4 hover:text-black transition-colors"
-        >
-          Prisma
-        </Link>
-        <Link
-          href="https://postgres-starter.vercel.app/"
-          className="font-medium underline underline-offset-4 hover:text-black transition-colors"
-        >
-          Starter
-        </Link>
-        <Link
-          href="https://postgres-drizzle.vercel.app/"
-          className="font-medium underline underline-offset-4 hover:text-black transition-colors"
-        >
-          Drizzle
-        </Link>
-      </div>
-
-      <div className="sm:absolute sm:bottom-0 w-full px-20 py-10 flex justify-between">
-        <Link href="https://vercel.com">
-          <Image
-            src="/vercel.svg"
-            alt="Vercel Logo"
-            width={100}
-            height={24}
-            priority
-          />
-        </Link>
-        <Link
-          href="https://github.com/vercel/examples/tree/main/storage/postgres-kysely"
-          className="flex items-center space-x-2"
-        >
-          <Image
-            src="/github.svg"
-            alt="GitHub Logo"
-            width={24}
-            height={24}
-            priority
-          />
-          <p className="font-light">Source</p>
-        </Link>
+      <h1 className="text-2xl font-bold mb-4">Scrum Tool</h1>
+      <div className="grid grid-cols-3 gap-4">
+        {['1', '2', '3', '5', '8', '13'].map((number) => (
+          <div className="card bg-white shadow-2xl rounded-lg p-6 transform transition duration-500 hover:scale-105" key={number}>
+            <div className="card-body">
+              <h5 className="card-title text-lg font-bold text-center">{number}</h5>
+              <div className="mt-4 border-t-2"></div>
+              <div className="flex justify-between items-center mt-4">
+                <div className="flex items-center">
+                  <img className="w-8 h-8 rounded-full" src="https://via.placeholder.com/150" alt="Avatar"/>
+                  <div className="ml-4 text-sm">
+                    <p className="text-gray-900 mb-2">Scrum Master</p>
+                    <p className="text-gray-600">Master of Scrum</p>
+                  </div>
+                </div>
+                <button className="px-2 py-1 bg-gray-600 text-white text-sm rounded hover:bg-gray-500">View</button>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </main>
   )
