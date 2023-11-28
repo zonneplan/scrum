6e823eea9f00389e730e04243ae48c346fe20157
Stijnvb <stijnvb@hotmail.com>
2023-11-28 19:52:47 +0100

    added template

--- app/votepage/page.tsx
+++ app/votepage/page.tsx
@@ -1,10 +1,13 @@
 import Image from 'next/image'
 import Link from 'next/link'
 
+export const preferredRegion = 'home'
+export const dynamic = 'force-dynamic'
+
 export default function page() {
     return (
         <main>
-            <h1> test pagina </h1>
+            
         </main>
     )
 }
