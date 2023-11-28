b89efa60804167693696639ca7d8748aac9c901f
chester790 <cbornhijm@roc-dev.com>
2023-11-28 20:54:59 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -29,13 +29,9 @@ export default function Home() {
   return (
     <main className="relative flex min-h-screen flex-col items-center justify-center">
     <h1>Scrum Poker</h1>
-    <Counter />
-    <form onSubmit={handleFormSubmit}>
-      <button>versturen</button>
-      </form>
       <div style={{backgroundColor:'lightGray', padding:20, borderRadius:20}}>
         <div>
-        <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+        <label htmlFor="Name" className="block text-sm font-medium leading-6 text-gray-900">
           Name
         </label>
         <div className="mt-2">
@@ -44,10 +40,10 @@ export default function Home() {
             name="name"
             id="name"
             className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            placeholder="you@example.com"
+            placeholder=" John Doe"
           />
           <div>
-        <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+        <label htmlFor="RoomCode" className="block text-sm font-medium leading-6 text-gray-900">
           RoomCode
         </label>
         <div className="mt-2">
@@ -56,12 +52,12 @@ export default function Home() {
             name="RoomCode"
             id="RoomCode"
             className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            placeholder="xlJKASD7"
+            placeholder=" xlJKASD7"
           />
         </div>
         <br></br>
         <Link style={{padding: 10, borderRadius: 8, backgroundColor: "#4CBB17"}} href={"/home"}> 
-            join
+            Join
         </Link>
       </div>
       </div>
