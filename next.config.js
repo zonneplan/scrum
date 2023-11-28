6ef5ad8d7cdb538243ee5ed919c736886d39c28f
Thomas-Vos00 <55391297+Thomas-Vos00@users.noreply.github.com>
2023-11-28 19:10:21 +0100

    Created a demo for pusher

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const nextConfig = {
   images: {
     remotePatterns: [{ hostname: 'images.ctfassets.net' }],
   },
+  env: {
+    PUSHER_APP_ID: process.env.PUSHER_APP_ID,
+    PUSHER_KEY: process.env.PUSHER_KEY,
+    PUSHER_SECRET: process.env.PUSHER_SECRET,
+    PUSHER_CLUSTER: process.env.PUSHER_CLUSTER,
+  }
 };
 
 module.exports = nextConfig;
