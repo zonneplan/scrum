b1eedc88b80bd486f77d5aeeb14721c784b23ef3
Tiemco <70142044+TiemcoM@users.noreply.github.com>
2023-11-28 18:49:56 +0100

    bimbimbambam

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <main className="relative flex min-h-screen flex-col items-center justify-center">
         <h1>Hello, World!</h1>
+
     </main>
   )
 }
