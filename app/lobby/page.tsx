4444d8f0c60f831bbc2a974e2aaf523eb42b820a
Simon <s1175667@student.windesheim.nl>
2023-11-28 20:27:04 +0100

    Refactor homepage

--- app/lobby/page.tsx
+++ app/lobby/page.tsx
@@ -1,10 +1,12 @@
+import LobbyPage from "@/components/custom components/Lobby";
+
 export const preferredRegion = 'home'
 export const dynamic = 'force-dynamic'
 
 export default function Lobby() {
     return (
         <div>
-
+            <LobbyPage/>
         </div>
     )
 }
--- components/custom components/HomePage.tsx
+++ components/custom components/HomePage.tsx
@@ -1,9 +1,8 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
 interface HomePageProps {
-
 }
 
 const HomePage: React.FC<HomePageProps> = () => {
@@ -22,49 +21,66 @@ const HomePage: React.FC<HomePageProps> = () => {
         console.log(username);
         console.log(roomCode);
 
-        //redirect to lobby
+        // Redirect to lobby using useNavigate
         window.location.href = '/lobby';
     };
 
     return (
-        <div className="flex items-center justify-center h-screen bg-gray-200">
-            <div className="bg-white p-8 rounded shadow-md w-96">
-                <h2 className="text-2xl font-semibold mb-4">Scrum Poker</h2>
-
-                <div className="mb-4">
-                    <label htmlFor="username" className="block text-sm font-medium text-gray-600">
-                        Username
-                    </label>
-                    <input
-                        type="text"
-                        id="username"
-                        name="username"
-                        value={username}
-                        onChange={handleUsernameChange}
-                        className="mt-1 p-2 w-full border rounded-md"
-                    />
+        // <Router>
+        <div className="bg-white py-16 sm:py-24 lg:py-32">
+            <div className="mx-auto grid max-w-7xl grid-cols-1 gap-10 px-6 lg:grid-cols-12 lg:gap-8 lg:px-8">
+                <div className="max-w-xl text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl lg:col-span-7">
+                    <h2 className="inline sm:block lg:inline xl:block">Zin in een potje poker?</h2>{' '}
+                    <p className="inline sm:block lg:inline xl:block">Vraag de code bij je teamgenoten!</p>
                 </div>
+                <form className="w-full max-w-md lg:col-span-5 lg:pt-2" onSubmit={(e) => e.preventDefault()}>
+                    <div className="flex gap-x-4">
+                        <label htmlFor="username" className="sr-only">
+                            Username
+                        </label>
+                        <input
+                            id="username"
+                            name="username"
+                            type="text"
+                            autoComplete="username"
+                            required
+                            value={username}
+                            onChange={handleUsernameChange}
+                            className="min-w-0 flex-auto rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            placeholder="John Doe"
+                        />
 
-                <div className="mb-4">
-                    <label htmlFor="roomCode" className="block text-sm font-medium text-gray-600">
-                        Room Code
-                    </label>
-                    <input
-                        type="text"
-                        id="roomCode"
-                        name="roomCode"
-                        value={roomCode}
-                        onChange={handleRoomCodeChange}
-                        className="mt-1 p-2 w-full border rounded-md"
-                    />
-                </div>
+                        <label htmlFor="roomCode" className="sr-only">
+                            Room Code
+                        </label>
+                        <input
+                            id="roomCode"
+                            name="roomCode"
+                            type="text"
+                            autoComplete="roomCode"
+                            required
+                            value={roomCode}
+                            onChange={handleRoomCodeChange}
+                            className="min-w-0 flex-auto rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            placeholder="1234"
+                        />
 
-                <button
-                    onClick={redirectToNextPage}
-                    className="bg-blue-500 text-white p-2 rounded-md w-full hover:bg-blue-600"
-                >
-                    Join Room
-                </button>
+                        <button
+                            type="button"
+                            onClick={redirectToNextPage}
+                            className="flex-none rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        >
+                            Join Room
+                        </button>
+                    </div>
+                    <p className="mt-4 text-sm leading-6 text-gray-900">
+                        Ben jij de baas? Maak een eigen{' '}
+                        <a href="newroom" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                            room&nbsp;
+                        </a>
+                        aan.
+                    </p>
+                </form>
             </div>
         </div>
     );
